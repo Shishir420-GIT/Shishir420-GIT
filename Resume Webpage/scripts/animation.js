6f45b62cb9a81184550855b9d5f83076c7e5c280
Shishir420-GIT <shishirsrivastav@gmail.com>
2023-08-05 23:47:28 +0530

    fixes

---
+++ Resume Webpage/scripts/animation.js
@@ -0,0 +1,6 @@
+//Setup and start animation! 
+
+var typed = new Typed('#element', {
+    strings: ['Developer', 'Engineer', 'Artist', 'Poet', 'Gym Guy', ' Basically A jack of all trades ;)'],
+    typeSpeed: 50,
+});
