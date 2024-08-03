040f22eafd21af8dedb8cabfc03e13756aaf950a
Shishir420-GIT <shishirsrivastav@gmail.com>
2024-08-03 13:18:09 +0530

    Updating Folder structure

--- Resume Webpage/scripts/animation.js
+++
@@ -1,6 +0,0 @@
-//Setup and start animation! 
-
-var typed = new Typed('#element', {
-    strings: ['Developer', 'Engineer', 'Artist', 'Poet', 'Gym Guy', ' Basically A jack of all trades ;)'],
-    typeSpeed: 50,
-});
